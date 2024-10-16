b7b99c217a5a16ca602c09a69074f727b98491ab
AdamBrown04 <adambrown2004@outlook.com>
2024-10-16 13:35:36 +0100

    completed the task

--- Y2WK4-Task/Program.cs
+++ Y2WK4-Task/Program.cs
@@ -1,13 +1,41 @@
 ﻿string path = "numbers.txt";
-List<int> numbers = new List<int>();
-
+List<string> fileContents = new List<string>();
 if (File.Exists(path))
 {
+    fileContents = File.ReadAllLines(path).ToList();
 
 }
+List<int> numbers = new List<int>();
+foreach(string number in fileContents)
+{
+    numbers.Add(Convert.ToInt32(number));
+}
 
-double average = 0;
+
 while (true)
 {
-    average = numbers.Average();
+    if(numbers.Count > 0)
+    {
+        Console.WriteLine($"The average is: {numbers.Average()}");
+    }
+   
+    Console.Write("Enter a new number: ");
+    string userInput = Console.ReadLine().ToLower();
+    if (userInput == "q")
+    {
+        break;
+    }
+    else
+    {
+        numbers.Add(Convert.ToInt32(userInput));
+    }
 }
+
+//update file
+List<string> fileNumbers = new List<string>();
+foreach (int number in numbers)
+{
+    fileNumbers.Add(number.ToString());
+}
+File.WriteAllLines(path, fileNumbers);
+Console.WriteLine("File updated \nProgram ended");
